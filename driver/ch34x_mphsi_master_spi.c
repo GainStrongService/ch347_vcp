c8045a9261e33bd60c57d7f9cc80439db50be33f
Joe Guo <joe@gainstrong.cn>
2024-01-11 15:39:19 +0800

    修改bitorder

--- driver/ch34x_mphsi_master_spi.c
+++ driver/ch34x_mphsi_master_spi.c
@@ -202,7 +202,7 @@ bool spicfg_to_hwcfg(mspi_cfgs *spicfg, stream_hw_cfgs *hwcfg)
 		return false;
 
 	spicfg->ibyteorder &= 0x01;
-	hwcfg->spi_initcfg.spi_firstbit = 0x00;
+	hwcfg->spi_initcfg.spi_firstbit = 0x80;
 
 	switch (spicfg->imode) {
 	case 0:
